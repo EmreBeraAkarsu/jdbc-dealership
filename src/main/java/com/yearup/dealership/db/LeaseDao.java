085bf6c870f7f7dc595e4cf6e8b66b44f71304a6
Emre Akarsu <akarsutech@gmail.com>
2024-12-10 19:46:59 -0500

    completed the sales and lease contract dao

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -15,6 +15,26 @@ public class LeaseDao {
     }
 
     public void addLeaseContract(LeaseContract leaseContract) {
-        // TODO: Implement the logic to add a lease contract
+        String query = "insert into lease_contracts (contract_id, vin, lease_start, lease_end, monthly_payment) Values (?, ?, ?, ?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, leaseContract.getContractId());
+            preparedStatement.setString(2, leaseContract.getVin());
+            preparedStatement.setDate(3, java.sql.Date.valueOf(leaseContract.getLeaseStart()));
+            preparedStatement.setDate(4, java.sql.Date.valueOf(leaseContract.getLeaseEnd()));
+            preparedStatement.setDouble(5, leaseContract.getMonthlyPayment());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Added the lease contract!");
+            } else {
+                System.out.println("Couldn't add the lease contract");
+            }
+        } catch (SQLException e) {
+
+            System.err.println("Error adding thge lease contract" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/yearup/dealership/db/SalesDao.java
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -15,6 +15,25 @@ public class SalesDao {
     }
 
     public void addSalesContract(SalesContract salesContract) {
-        // TODO: Implement the logic to add a sales contract
+        String query = "insert into sales_contract (contract_id, vin, sale_date, price) Values (?, ?, ?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, salesContract.getContractId());
+            preparedStatement.setString(2, salesContract.getVin());
+            preparedStatement.setDate(3, java.sql.Date.valueOf(salesContract.getSaleDate()));
+            preparedStatement.setDouble(4, salesContract.getPrice());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Added the lease contract!");
+            } else {
+                System.out.println("Couldn't add the lease contract");
+            }
+        } catch (SQLException e) {
+
+            System.err.println("Error adding thge lease contract" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
